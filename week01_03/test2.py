f51f2c8a5bc86bf36e3188f1ae71e2dffd6949bc
NikolaPavlov <nikola.pavlov1955@gmail.com>
2015-03-27 10:24:37 +0200

    from softuni from laptop

--- week01_03/test2.py
+++ week01_03/test2.py
@@ -1,7 +1,7 @@
 #regex shits
 import re
 
-txt = "This is some txt for regex text Bitch!!   $zzz"
+txt = "This is some txt for regex text !!!   $zzz"
 matches = re.search("[a-zA-Z]+", txt)
 matches1 = re.search("[/w]", txt)
 
