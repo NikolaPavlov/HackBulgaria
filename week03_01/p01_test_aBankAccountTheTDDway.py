b1db6c94875356ef9ee0689f9923258324666756
NikolaPavlov <nikola.pavlov1955@gmail.com>
2015-04-01 23:46:32 +0300

    typo

--- week03_01/p01_test_aBankAccountTheTDDway.py
+++ week03_01/p01_test_aBankAccountTheTDDway.py
@@ -2,42 +2,30 @@ from p01_aBankAccountTheTDDway import *
 
 import unittest
 
+
 class BankAccTests(unittest.TestCase):
+
     def setUp(self):
         self.acc = BankAcc("GoGo!", 512, '$')
 
-
     def testConstructor(self):
         self.assertTrue(isinstance(self.acc, BankAcc))
-
 
     def testDepositMoney(self):
         with self.assertRaises(ValueError):
             self.acc.deposit_money(-111)
 
-
     def testShowBalance(self):
         self.assertTrue(self.acc.balance(), self.acc.balance)
 
-
     def testWithdrawMoney(self):
         with self.assertRaises(ValueError):
-            with self.acc.withdraw_money(99999)
-
 
     def test_to_str(self):
         self.assertEqual(str(self.acc), "Bank account for GoGo with balance of 512$")
 
-
     def test_int_value(self):
         self.assertEqual(int(self.acc), 512)
-
-
-
-
-
-
-
 
 
 if __name__ == "__main__":
