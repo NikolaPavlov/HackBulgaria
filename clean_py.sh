5e342e4dd2488e54012147f4cc96aa9cb5e2d7e7
NikolaPavlov <nikola.pavlov1955@gmail.com>
2015-05-05 22:20:31 +0300

    remove all py cache with script

---
+++ clean_py.sh
@@ -0,0 +1,12 @@
+#!/b:in/sh
+
+# recursively removes all .pyc files and __pycache__ directories in the current
+# directory
+
+find . | \
+  grep -E "(__pycache__|\.pyc$)" | \
+  xargs rm -rf
+
+# or, for copy-pasting:
+# find . | grep -E "(__pycache__|\.pyc$)" | xargs rm -rf
+
