879589156e54640bf87c005c1d99b270231ac4e1
NikolaPavlov <nikola.pavlov1955@gmail.com>
2015-05-22 09:28:53 +0300

    added bootstrap to cinema_django

--- week10/cinema_django/website/views.py
+++ week10/cinema_django/website/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponse, HttpResponseNotFound
+from .models import Movie, Projection, Reservation
+
 
 # Create your views here.
+def index(request):
+    movies = Movie.objects.all()
+    return render(request, 'base.html', locals())
